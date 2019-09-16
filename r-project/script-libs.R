70641b8739ade897807fd78fedbceb16da78fb11
unknown <viniciuscasani3@gmail.com>
2019-09-16 20:37:34 -0300

    adicionado tfidf

--- r-project/script-libs.R
+++ r-project/script-libs.R
@@ -3,3 +3,7 @@ install.packages("httr", dependencies = TRUE)
 
 #pacotes para manipular JSON
 install.packages(c("rlist", "jsonlite"), dependencies = TRUE)
+
+#pacote para vetorizar dados
+install.packages("text2vec", dependencies = TRUE)
+install.packages("superml", dependencies = TRUE)
---
+++ r-project/vetorizacao.R
@@ -0,0 +1,11 @@
+#carrega arquivos
+source("request_server.R")
+
+library(text2vec)
+
+library(superml)
+
+dataset = dataset()
+
+View(dataset)
+
